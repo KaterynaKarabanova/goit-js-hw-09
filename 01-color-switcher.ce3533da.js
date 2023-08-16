28198dc9e0a888c6087dfa6ced7f4960cc0fdbb1
KaterynaKarabanova <135024704+KaterynaKarabanova@users.noreply.github.com>
2023-08-16 13:07:22 +0000

    Deploying to gh-pages from @ KaterynaKarabanova/goit-js-hw-09@b6bfaacccbe973fedd437732b3756d5d2bed7962 🚀

---
+++ 01-color-switcher.ce3533da.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n,r=!1;t.addEventListener("click",(function(){r||(r=!0,n=setInterval((()=>o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`),1e3))})),e.addEventListener("click",(function(){r&&(r=!1,clearInterval(n))}));
+//# sourceMappingURL=01-color-switcher.ce3533da.js.map
